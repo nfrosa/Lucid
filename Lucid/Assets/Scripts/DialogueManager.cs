7ccca05324d25b211feb730600a203df3d7cfa3f
Mir Ahmed <mirahmed753@gmail.com>
2018-03-26 12:37:43 -0400

    Completed Dialogue Work

--- Lucid/Assets/Scripts/DialogueManager.cs
+++ Lucid/Assets/Scripts/DialogueManager.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
+	public Text nameText;
+	public Text dialogueText;
 	private Queue<string> sentences;
 
 	// Use this for initialization
@@ -11,7 +14,29 @@ public class DialogueManager : MonoBehaviour {
 	}
 
 	public void StartDialogue(Dialogue dialogue) {
-		Debug.Log("Starting conversation with " + dialogue.name);
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences) {
+			sentences.Enqueue (sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence() {
+		if (sentences.Count == 0) {
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue ();
+		dialogueText.text = sentence;
+	}
+
+	void EndDialogue() {
+		Debug.Log("End of Conversation");
 	}
 
 }
