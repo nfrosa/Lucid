a576d0c432a17eec47e85bdb25b0cb788e319fdb
Mir Ahmed <mirahmed753@gmail.com>
2018-03-28 19:21:04 -0400

    stash dem

---
+++ Lucid/Assets/Sprites/Enemy sprites/WitchController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchController : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.name == "lucid_dreamSprites_0") {
+			Destroy (other.gameObject);
+		}
+	}
+}
