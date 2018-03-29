138c49955b1e173eef02309433dc58fa45e670f1
Mir Ahmed <mirahmed753@gmail.com>
2018-03-29 14:14:40 -0400

    Player Death and Reloading

--- Lucid/Assets/Sprites/Enemy sprites/WitchController.cs
+++ Lucid/Assets/Sprites/Enemy sprites/WitchController.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WitchController : MonoBehaviour {
+	public float waitToReload;
+	private bool reloading; 
+	private GameObject thePlayer;
+
+	void Update() {
+		if (reloading) {
+			waitToReload -= Time.deltaTime;
+			if (waitToReload < 0) {
+				Application.LoadLevel(Application.loadedLevel);
+				thePlayer.SetActive(true);
+			}
+		}
+	}
+
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.name == "lucid_dreamSprites_0") {
-			Destroy (other.gameObject);
+			other.gameObject.SetActive(false);
+			reloading = true;
+			thePlayer = other.gameObject;
 		}
 	}
 }
