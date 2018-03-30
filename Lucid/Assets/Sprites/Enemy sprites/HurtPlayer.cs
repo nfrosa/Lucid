71d5eb12654d9d834c6c9f1b66a6ec6389646f97
Michael Samper <msamper@ufl.edu>
2018-03-30 11:48:14 -0400

    added punches

--- Lucid/Assets/Sprites/Enemy sprites/HurtPlayer.cs
+++ Lucid/Assets/Sprites/Enemy sprites/HurtPlayer.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class HurtPlayer : MonoBehaviour {
 
 	public int damageToGive;
+	public int curHealth;
+	public int maxHealth = 30;
+
+	void Start()
+	{
+		curHealth = maxHealth;
+	}
+
+	void Update()
+	{
+		if (curHealth <= 0) {
+			Destroy (gameObject);
+		}
+	}
 
 	void OnCollisionEnter2D(Collision2D other) {
 		if(other.gameObject.tag == "Player") {
 			other.gameObject.GetComponent<PlayerHealthManager> ().HurtPlayer(damageToGive);
 		}
+	}
+
+	public void Damage(int damage){
+		curHealth -= damage;
+
 	}
 }
