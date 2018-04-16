632792c9da1128b39c5ae0a0c149c49d958e0319
Jenny <jennytong@ufl.edu>
2018-04-16 13:45:31 -0400

    Added main menu linking it to dream world scene

---
+++ Lucid/Assets/Lvl1 Puzzle/Scripts/ButtonQuit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonQuit : MonoBehaviour {
+	public Button myButton;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = myButton.GetComponent<Button>();
+        btn.onClick.AddListener(switchScene);
+	}
+
+	void switchScene () {
+		Application.Quit();
+	}
+}
